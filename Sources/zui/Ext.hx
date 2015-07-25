46a011e1886c628cd40dc7d4c6dc656c3420ac9c
Lubos Lenco <lubos.lenco@gmail.com>
2015-07-25 16:40:41 +0200

    Editable list callbacks

--- Sources/zui/Ext.hx
+++ Sources/zui/Ext.hx
@@ -2,20 +2,25 @@ package zui;
 
 class Ext {
 
-	public static function drawEditableList(ui:Zui, id:String, ar:Array<String>, itemCb:Int->Void = null) {
+	public static function drawEditableList(ui:Zui, id:String, ar:Array<String>,
+                                            addCb:Void->Void = null, removeCb:Int->Void = null,
+                                            itemDrawCb:String->Int->Void = null) {
         var i = 0;
         while (i < ar.length) {
             ui.row([0.8, 0.2]);
-            ar[i] = ui.textInput(Id.nest(id, i), ar[i]);
+            var itemId = Id.nest(id, i);
+            ar[i] = ui.textInput(itemId, ar[i]);
             if (ui.button("X")) {
                 ar.splice(i, 1);
+                if (removeCb != null) removeCb(i);
             }
             else i++;
 
-            if (itemCb != null) itemCb(i - 1);
+            if (itemDrawCb != null) itemDrawCb(Id.nest(itemId, i), i - 1);
         }
         if (ui.button("Add")) {
             ar.push("untitled");
+            if (addCb != null) addCb();
         }
     }
 }
