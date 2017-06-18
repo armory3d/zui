3fac65f0a0c98f6e241791ab9e7d30d9a4e7e5e1
Luke <selman.luke1@gmail.com>
2017-06-18 02:18:17 +0100

    Re-add accidental removal of Return handler

--- Sources/zui/Zui.hx
+++ Sources/zui/Zui.hx
@@ -428,6 +428,8 @@ class Zui {
 						cursorX--;
 					}
 				}
+				else if (key == kha.input.KeyCode.Return) { // Deselect
+ -					deselectText(); // One-line text for now
 				else if (key == kha.input.KeyCode.CapsLock) {
 					// dummy check to prevent previous character from being entered accidentally
 					// terrible hack... should improve
