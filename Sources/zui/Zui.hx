851d6b492082591d498fb9bf4bfacb5c7ae08ecf
luboslenco <lubos.lenco@gmail.com>
2019-06-18 08:30:05 +0200

    Fire copy cut paste once

--- Sources/zui/Zui.hx
+++ Sources/zui/Zui.hx
@@ -63,7 +63,6 @@ class Zui {
 	static var isCopy = false;
 	static var isPaste = false;
 	static var copyReceiver: Zui = null;
-	static var copyFrame = 0;
 
 	var cursorX = 0; // Text input
 	var cursorY = 0;
@@ -159,7 +158,7 @@ class Zui {
 			kha.System.notifyOnCutCopyPaste(onCut, onCopy, onPaste);
 			kha.System.notifyOnFrames(function(frames: Array<kha.Framebuffer>) {
 				// Set isCopy to false on next frame
-				if ((isCopy || isPaste) && ++copyFrame > 1) { isCopy = isCut = isPaste = false; copyFrame = 0; }
+				isCopy = isCut = isPaste = false;
 			});
 		}
 	}
