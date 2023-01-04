ab6770286aca48c8c2b3d8350eb65ff61ad40a53
luboslenco <lubos.lenco@gmail.com>
2023-01-04 21:34:01 +0100

    Remove MultipleWindows example as it does not compile anymore

--- examples/MultipleWindows/README.md
+++
@@ -1,3 +0,0 @@
-# MultipleWindows
-- sample to show Zui usage in multiple windows
-- requires a Kha target that actually supports that (only windows/linux with opengl for now)
--- examples/MultipleWindows/khafile.js
+++
@@ -1,7 +0,0 @@
-let project = new Project('Elements');
-
-project.addSources('Sources');
-project.addAssets('../SharedAssets/DroidSans.ttf');
-project.addLibrary('../../../../zui');
-
-resolve(project);
